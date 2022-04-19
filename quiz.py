52381e928140449704f60390d29f494492c1b5e3
eight <c0b2106880@edu.teu.ac.jp>
2022-04-19 17:53:00 +0900

    練習1修正版

--- quiz.py
+++ quiz.py
@@ -1,8 +1,10 @@
 from importlib import import_module
 from random import randint
 import datetime
+
 st = datetime.datetime.now()
 que = ["サザエの旦那の名前は？","カツオの妹の名前は？","タラオはカツオから見てどんな関係？"]
+
 num = randint(0,2)
 print(que[num])
 
@@ -10,17 +12,24 @@ ans = input("回答を入力：")
 if num == 0:
     if ans == "ますお" or "マスオ":
         print("正解")
+
     else:
         print("不正解")
-if num == 1:
+
+elif num == 1:
     if ans == "わかめ" or "ワカメ":
         print("正解")
+
     else:
         print("不正解")
-if num == 2:
+
+elif num == 2:
     if ans == "甥" or "おい" or "甥っ子" or "おいっこ":
         print("正解")
+
     else:
         print("不正解")
+
+        
 ed = datetime.datetime.now()
 print((ed-st).seconds)
