b8c1e61c34b4d6c232e6039aa69e24b382d918ac
eight <c0b2106880@edu.teu.ac.jp>
2022-04-26 18:01:50 +0900

    変更2

--- rensyu02/calc.py
+++ rensyu02/calc.py
@@ -10,11 +10,11 @@ def button_click(event):
 def button_equal(event):
     get = entry.get()
     res = eval(get)
-    entry.delete(0,tk.END)
-    entry.insert(tk.END,res)
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, res)
 
 def button_clear(event):
-    entry.delete(0,tk.END)
+    entry.delete(0, tk.END)
 
 root = tk.Tk()
 root.title("tk")
@@ -24,26 +24,26 @@ x =1
 y =0
 for i in range(9, -1, -1):
     button = tk.Button(root,text=i,font=("Times New Roman", 30))
-    button.bind("<1>",button_click)
+    button.bind("<1>", button_click)
     button.grid(row = x, column = y, padx = 10, pady = 10)
     y += 1
     if y == 3:
         x += 1
         y = 0
 
-button = tk.Button(root,text="+",font=("Times New Roman", 30))
-button.bind("<1>",button_click)
+button = tk.Button(root, text="+",font=("Times New Roman", 30))
+button.bind("<1>", button_click)
 button.grid(row = 4, column = 1)
 
-button = tk.Button(root,text="=",font=("Times New Roman", 30))
-button.bind("<1>",button_equal)
+button = tk.Button(root, text="=", font=("Times New Roman", 30))
+button.bind("<1>", button_equal)
 button.grid(row = 4, column = 2)
 
-button = tk.Button(root,text="C",font=("Times New Roman", 30))
-button.bind("<1>",button_clear)
+button = tk.Button(root, text="C", font=("Times New Roman", 30))
+button.bind("<1>", button_clear)
 button.grid(row = 4, column = 3)
 
-entry = tk.Entry(root,justify="right",width=10,font=("Times New Roman",40))
-entry.grid(row = 0, column = 0,columnspan = 3)
+entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman",40))
+entry.grid(row = 0, column = 0, columnspan = 3)
 
 root.mainloop()
