c051cfc1b376e79b076eee50bd5ae247078688dd
eight <c0b2106880@edu.teu.ac.jp>
2022-05-10 16:48:18 +0900

    壁判定完成版

---
+++ rensyu03/maze.py
@@ -0,0 +1,56 @@
+import tkinter as tk
+import maze_maker
+
+def key_down(event):
+    global key
+    key = event.keysym
+
+def key_up(event):
+    global key
+    key = ""
+
+def main_proc():
+    global cx, cy, mx, my, key
+    if key == "Up" and x[my-1][mx] == 0:
+        my -= 1
+    if key == "Down" and x[my+1][mx] == 0:
+        my += 1
+    if key == "Left" and x[my][mx-1] == 0:
+        mx -= 1
+    if key == "Right" and x[my][mx+1] == 0:
+        mx += 1
+    
+    '''if key == "Up":
+        cy -= 100
+    if key == "Down":
+        cy += 100
+    if key == "Left":
+        cx -= 100
+    if key == "Right":
+        cx += 100'''
+    
+    cx, cy = 100*mx+50, 100*my+50
+    canvas.coords("tori", cx, cy)
+    root.after(100, main_proc)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("迷えるこうかとん")
+
+    canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
+    canvas.pack()
+
+    x = maze_maker.make_maze(15, 9)
+    maze_maker.show_maze(canvas, x)
+
+    tori = tk.PhotoImage(file = "fig/8.png")
+    mx, my = 1,1
+    cx, cy = 100*mx+50, 100*my+50
+    canvas.create_image(cx, cy, image = tori, tag = "tori")
+    key = ""
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
+
+    root.after(100, main_proc)
+    root.mainloop()
